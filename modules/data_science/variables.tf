84b8093abdec739a32292d1803cb06505cb38650
victorakh <133075156+victorakh@users.noreply.github.com>
2024-01-18 15:48:49 +0800

    Update variables.tf

--- modules/data_science/variables.tf
+++ modules/data_science/variables.tf
@@ -285,8 +285,12 @@ variable "trusted_users" {
   default     = []
 }
 
+
+
+#Hide this as I'm fixing the region/zone bug by hardcoding in main.tf so no point displaying this option
 variable "zone" {
-  description = "Cloud Zone associated to the AI Notebooks. List of zones - https://cloud.google.com/compute/docs/regions-zones#available. {{UIMeta group=0 order=20 }}"
+  #description = "Cloud Zone associated to the AI Notebooks. {{UIMeta group=2 order=3 options=asia-southeast1-a,asia-southeast1-b,asia-southeast1-c }}"
+  description = "Cloud Zone associated to the AI Notebooks. {{UIMeta group=0 order=3 options=asia-southeast1-a,asia-southeast1-b,asia-southeast1-c }}"
   type        = string
   default     = "asia-southeast1-a"
 }
