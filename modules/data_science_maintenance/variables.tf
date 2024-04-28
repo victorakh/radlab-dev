4a06bcfba578fe0e58139d3bc7afb69398cf6620
victorakh <133075156+victorakh@users.noreply.github.com>
2024-04-28 18:08:13 +0800

    Update variables.tf

--- modules/data_science_maintenance/variables.tf
+++ modules/data_science_maintenance/variables.tf
@@ -88,7 +88,14 @@ variable "billing_budget_pubsub_topic" {
 }
 
 variable "boot_disk_size_gb" {
-  description = "The size of the boot disk in GB attached to this instance.  {{UIMeta group=3 order=8 options=200 }}"
+  description = "The size of the boot disk in GB attached to this instance.  {{UIMeta group=3 order=8 options=100 }}"
+  type        = number
+  default     = 100
+}
+
+
+variable "data_disk_size_gb" {
+  description = "The size of the data disk in GB attached to this instance.  {{UIMeta group=3 order=8 options=200 }}"
   type        = number
   default     = 200
 }
