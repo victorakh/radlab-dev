825fcc24acd4ac53c8415d5e69d7fc3f0837e5af
victorakh <133075156+victorakh@users.noreply.github.com>
2024-04-29 13:05:25 +0800

    Update main.tf

--- modules/data_science_maintenance/main.tf
+++ modules/data_science_maintenance/main.tf
@@ -199,7 +199,8 @@ resource "null_resource" "ai_notebook_usermanaged_provisioning_state" {
 resource "google_notebooks_instance" "ai_notebook_usermanaged" {
   count        = var.notebook_count > 0 && var.create_usermanaged_notebook ? var.notebook_count : 0
   project      = local.project.project_id
-  name         = "usermanaged-notebooks-${count.index + 1}"
+  name         = "umnb-${count.index + 1}-var.project_id_prefix"         // Amend - usermanaged notebook name -  "umnb-1-projectname
+#  name         = "usermanaged-notebooks-${count.index + 1}"
   location     = "asia-southeast1-a"
   machine_type = var.machine_type
 
